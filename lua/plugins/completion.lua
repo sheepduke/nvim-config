1c20f15e14cb3f28469d90e8b71504faa84f14de
YUE Daian <sheepduke@gmail.com>
2023-06-13 02:03:23 +0800

    feat: add cmd completion

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -10,7 +10,7 @@ return {
       "neovim/nvim-lspconfig"
     },
     config = function()
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
       -- Advertise capabilities to `clangd`.
       local lspconfig = require("lspconfig")
@@ -22,9 +22,31 @@ return {
   },
   -------------------------------------------------------------------
   {
+    "hrsh7th/cmp-cmdline",
+    config = function()
+      local cmp = require("cmp")
+
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = "path" }
+        }, {
+          {
+            name = "cmdline",
+            option = {
+              ignore_cmds = { "Man", "!" }
+            }
+          }
+        })
+      })
+    end
+  },
+  -------------------------------------------------------------------
+  {
     "hrsh7th/nvim-cmp",
     dependencies = {
       "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-cmdline",
     },
     config = function()
       local cmp = require("cmp")
@@ -35,16 +57,18 @@ return {
           end
         },
         mapping = cmp.mapping.preset.insert({
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<C-e>'] = cmp.mapping.abort(),
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
           -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
         }),
-        sources = {
+        sources = cmp.config.sources({
           { name = "nvim_lsp" },
-        }
+        }, {
+          { name = "buffer" }
+        })
       }
     end
   }
