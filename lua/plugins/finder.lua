eec8ee1c348a619db53fcf6bc5d939c32dc45782
YUE Daian <sheepduke@gmail.com>
2023-06-13 23:47:10 +0800

    feat: add telescope

---
+++ lua/plugins/finder.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "nvim-telescope/telescope-fzf-native.nvim",
+    build =
+    "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build"
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim"
+    },
+    config = function()
+      local telescope = require("telescope")
+      telescope.load_extension("fzf")
+    end
+  }
+}
