c94165bf0df1488b1fc3f042bb5aef8a23a059cd
YUE Daian <sheepduke@gmail.com>
2023-06-17 14:56:53 +0800

    feat: implement open with external program

--- lua/plugins/file-manager.lua
+++ lua/plugins/file-manager.lua
@@ -11,6 +11,39 @@ return {
     config = function()
       -- Remove the deprecated commands from v1.x
       vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+
+      require("neo-tree").setup {
+        filesystem = {
+          window = {
+            mappings = {
+              ["o"] = "openWithExternalProgram"
+            },
+          },
+        },
+        commands = {
+          openWithExternalProgram = function(state)
+            local node = state.tree:get_node()
+            local path = node:get_id()
+
+            local fileTypeHandler = {
+              ["m4v"] = "mpv %s",
+              ["mkv"] = "mpv %s",
+              ["mp4"] = "mpv %s"
+            }
+
+            local fileType = string.match(path, "[^.]+$")
+            local handler = fileTypeHandler[fileType]
+            if handler == nil then
+              vim.notify(
+                string.format("Unable to find a registered program for file type '%s'", fileType),
+                vim.log.levels.ERROR)
+            else
+              local command = string.format(handler, path)
+              vim.fn.jobstart(command)
+            end
+          end
+        }
+      }
     end
   },
   --------------------------------------------------------------------
